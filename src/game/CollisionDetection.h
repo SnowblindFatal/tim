e94cc090f7d7e71cd43e95dd71c893a28127c211
Väyrynen Joni <vayrynj2@cc.hut.fi>
2013-11-30 12:52:29 +0200

    collison detection update

--- src/game/CollisionDetection.h
+++ src/game/CollisionDetection.h
@@ -6,25 +6,27 @@ class CollisionDetection : public b2ContactListener {
 	void BeginContact(b2Contact* contact) {
 		//check if fixture A was a bomb
 		void* bodyUserData = contact->GetFixtureA()->GetBody()->GetUserData();
-		if ( bodyUserData )
-			static_cast<Bomb*>( bodyUserData )->startContact();
-
+		if ( bodyUserData ) {
+			static_cast<Bomb*>( bodyUserData )->explode();
+		}
 		//check if fixture B was a bomb
 		bodyUserData = contact->GetFixtureB()->GetBody()->GetUserData();
-		if ( bodyUserData )
-			static_cast<Bomb*>( bodyUserData )->startContact();
+		if ( bodyUserData ) {
+			static_cast<Bomb*>( bodyUserData )->explode();
+		}
 	}
-
+/*
 	void EndContact(b2Contact* contact) {
-		//check if fixture A was a bomb
+		
 		void* bodyUserData = contact->GetFixtureA()->GetBody()->GetUserData();
 		if ( bodyUserData )
 			static_cast<Bomb*>( bodyUserData )->endContact();
-		//check if fixture B was a bomb
+		
 		bodyUserData = contact->GetFixtureB()->GetBody()->GetUserData();
 		if ( bodyUserData )
 			static_cast<Bomb*>( bodyUserData )->endContact();
 	}
+*/
 };
 
 
